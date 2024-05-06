2bc3feb16e20c74ecdaf8670243889f6ffd0e3e4
MajdKadour <mkadour335@gmail.com>
2024-05-06 11:24:17 +0300

    Add files via upload

---
+++ rabit game/rabit game/rabit_game.py
@@ -0,0 +1,19 @@
+﻿lis_herbs=[["🍀","🍀","🍀"],["🍀","🍀","🍀"],["🍀","🍀","🍀"]]
+
+print("welcome to place the rabit")
+print(lis_herbs[0])
+print(lis_herbs[1])
+print(lis_herbs[2])
+
+print("wher should rabit go? 🐇")
+row_columan=input("please choose row and column")
+
+row=int(row_columan[0])-1
+columan=int(row_columan[1])-1
+lis_herbs[row][columan]="🐇"
+
+print(lis_herbs[0])
+print(lis_herbs[1])
+print(lis_herbs[2])
+
+
